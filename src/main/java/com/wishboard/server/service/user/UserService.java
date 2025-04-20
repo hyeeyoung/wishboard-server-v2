bd4f8a0a07cae9cbf9d0182317bfaa46bfefe248
hyejungg <kimhyejung12@naver.com>
2025-04-20 15:49:38 +0900

    [mod] 이미지 수정 시 기존에 있는 이미지 삭제하고 추가하도록

--- src/main/java/com/wishboard/server/service/user/UserService.java
+++ src/main/java/com/wishboard/server/service/user/UserService.java
@@ -1,9 +1,5 @@
 package com.wishboard.server.service.user;
 
-import io.micrometer.common.util.StringUtils;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,56 +17,64 @@ import com.wishboard.server.service.image.provider.dto.request.ImageUploadFileRe
 import com.wishboard.server.service.user.dto.CreateUserDto;
 import com.wishboard.server.service.user.dto.UserDto;
 
+import io.micrometer.common.util.StringUtils;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
 @RequiredArgsConstructor
 @Service
 @Transactional
 public class UserService {
 
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-    private final S3Provider s3Provider;
-    private final ModelMapper modelMapper;
+	private final S3Provider s3Provider;
+	private final ModelMapper modelMapper;
 
-    public Long registerSocialUser(CreateUserDto request) {
-        UserServiceUtils.validateNotExistsUser(userRepository, request.getSocialId(), request.getSocialType());
-        User user = userRepository.save(
-            User.newInstance(request.getSocialId(), AuthType.SOCIAL, request.getSocialType()));
-        return user.getId();
-    }
+	public Long registerSocialUser(CreateUserDto request) {
+		UserServiceUtils.validateNotExistsUser(userRepository, request.getSocialId(), request.getSocialType());
+		User user = userRepository.save(
+			User.newInstance(request.getSocialId(), AuthType.SOCIAL, request.getSocialType()));
+		return user.getId();
+	}
 
-    public UserDto getUserInfo(Long userId) {
-        var user = UserServiceUtils.findUserById(userRepository, userId);
-        return modelMapper.map(user, UserDto.class);
-    }
+	public UserDto getUserInfo(Long userId) {
+		var user = UserServiceUtils.findUserById(userRepository, userId);
+		return modelMapper.map(user, UserDto.class);
+	}
 
-    public UserDto updatePushState(Long userId, boolean pushState) {
-        var user = UserServiceUtils.findUserById(userRepository, userId);
-        user.updatePushState(pushState);
-        return modelMapper.map(user, UserDto.class);
-    }
+	public UserDto updatePushState(Long userId, boolean pushState) {
+		var user = UserServiceUtils.findUserById(userRepository, userId);
+		user.updatePushState(pushState);
+		return modelMapper.map(user, UserDto.class);
+	}
 
-    // TODO 이미지 업로드까지 정상적으로 잘 되는지 확인 필요!!!
-    public UserDto updateUserInfo(Long userId, UpdateUserInfoRequest request, MultipartFile image) {
-        var user = UserServiceUtils.findUserById(userRepository, userId);
-        user.updateUserNickname(request.getNickname());
-        if (image != null && !image.isEmpty()) {
-            String profileImageUrl = s3Provider.uploadFile(ImageUploadFileRequest.of(FileType.PROFILE_IMAGE), image);
-            if (StringUtils.isNotBlank(profileImageUrl)) {
-                user.updateProfileImage(image.getOriginalFilename(), profileImageUrl);
-            }
-        }
-        return modelMapper.map(user, UserDto.class);
-    }
+	public UserDto updateUserInfo(Long userId, UpdateUserInfoRequest request, MultipartFile image) {
+		var user = UserServiceUtils.findUserById(userRepository, userId);
+		user.updateUserNickname(request.getNickname());
+		if (image != null && !image.isEmpty()) {
+			String previousImageUrl = user.getProfileImgUrl();
+			if (StringUtils.isNotBlank(previousImageUrl)) {
+				s3Provider.deleteFile(previousImageUrl);
+			}
 
-    public UserDto updatePassword(Long userId, @Valid UpdatePasswordRequest request) {
-        var user = UserServiceUtils.findUserById(userRepository, userId);
-        String hashedPassword = AuthServiceUtils.getHashedPassword(request.getNewPassword());
-        user.updatePassword(hashedPassword);
-        return modelMapper.map(user, UserDto.class);
-    }
+			String profileImageUrl = s3Provider.uploadFile(ImageUploadFileRequest.of(FileType.PROFILE_IMAGE), image);
+			if (StringUtils.isNotBlank(profileImageUrl)) {
+				user.updateProfileImage(image.getOriginalFilename(), profileImageUrl);
+			}
+		}
+		return modelMapper.map(user, UserDto.class);
+	}
 
-    public void deleteUser(Long userId) {
-        var user = UserServiceUtils.findUserById(userRepository, userId);
-        userRepository.delete(user);
-    }
+	public UserDto updatePassword(Long userId, @Valid UpdatePasswordRequest request) {
+		var user = UserServiceUtils.findUserById(userRepository, userId);
+		String hashedPassword = AuthServiceUtils.getHashedPassword(request.getNewPassword());
+		user.updatePassword(hashedPassword);
+		return modelMapper.map(user, UserDto.class);
+	}
+
+	public void deleteUser(Long userId) {
+		var user = UserServiceUtils.findUserById(userRepository, userId);
+		userRepository.delete(user);
+	}
 }
