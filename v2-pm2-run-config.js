a37b46abd51e106ee67367c1c17085938b70115f
hyejungg <kimhyejung12@naver.com>
2025-06-21 17:36:24 +0900

    [fix] 실행 경로 수정

--- v2-pm2-run-config.js
+++ v2-pm2-run-config.js
@@ -2,6 +2,6 @@ module.exports = {
     apps: [{
         name: 'v2-api-server',
         script: 'java',
-        args: ['-jar', './server-2.0.0.jar'] // TODO 버전 업데이트 시 같이 반영
+        args: ['-jar', './build/libs/server-2.0.0.jar'] // TODO 버전 업데이트 시 같이 반영
     }],
 };
