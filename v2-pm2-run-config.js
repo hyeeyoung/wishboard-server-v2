093acf57462ce321ec0dc2f45c252b75e91945b9
hyejungg <kimhyejung12@naver.com>
2025-06-23 23:58:43 +0900

    [mod] 실행 명령어 수정

--- v2-pm2-run-config.js
+++ v2-pm2-run-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apps: [{
         script: 'java',
-        args: ['-jar', './build/libs/server-2.0.0.jar'] // TODO 버전 업데이트 시 같이 반영
+        args: ['-jar', './server-2.0.0.jar'] // TODO 버전 업데이트 시 같이 반영
     }],
 };
