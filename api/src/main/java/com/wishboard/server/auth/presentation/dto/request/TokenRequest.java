9e3e83b4ef9036b345821cf1832361981ee13b9f
hyejungg <kimhyejung12@naver.com>
2025-07-01 22:38:43 +0900

    [mod] deprecated 코드 삭제

--- api/src/main/java/com/wishboard/server/auth/presentation/dto/request/TokenRequest.java
+++ api/src/main/java/com/wishboard/server/auth/presentation/dto/request/TokenRequest.java
@@ -7,11 +7,11 @@ import jakarta.validation.constraints.NotBlank;
 
 public record TokenRequest(
 
-	@Schema(description = "토큰 - accessToken", example = "eyJhbGciOiJIUzUxMiJ9.eyJVU0VSX0lEIjoxLCJleHAiOjE2NTg4NDA2NzN9.udnKnDSK08EuX56E5k-vkYUbZYofuo12YdiM9gEPY4eqdfzM_xt4MpgTimTuQ8ipmMxWZNCaTjtentg8vLyfgQ", required = true)
+	@Schema(description = "토큰 - accessToken", example = "eyJhbGciOiJIUzUxMiJ9.eyJVU0VSX0lEIjoxLCJleHAiOjE2NTg4NDA2NzN9.udnKnDSK08EuX56E5k-vkYUbZYofuo12YdiM9gEPY4eqdfzM_xt4MpgTimTuQ8ipmMxWZNCaTjtentg8vLyfgQ")
 	@NotBlank(message = "{auth.accessToken.notBlank}")
 	String accessToken,
 
-	@Schema(description = "토큰 - refreshToken", example = "eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2NTk0NDM2NzN9.1L4eWqLGvob8jsTe5ZQVbmWpitVjZ0wMIoYRg6qPyum1iLaVOV_AT6nM0FtO5OrMM_9VXRWzMaON2S4E_QsxzQ", required = true)
+	@Schema(description = "토큰 - refreshToken", example = "eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2NTk0NDM2NzN9.1L4eWqLGvob8jsTe5ZQVbmWpitVjZ0wMIoYRg6qPyum1iLaVOV_AT6nM0FtO5OrMM_9VXRWzMaON2S4E_QsxzQ")
 	@NotBlank(message = "{auth.refreshToken.notBlank}")
 	String refreshToken
 ) {
