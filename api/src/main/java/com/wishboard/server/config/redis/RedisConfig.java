6c6bace9d97c317069e02c51e37cac243d6a0e9c
hyejungg <kimhyejung12@naver.com>
2025-07-02 00:49:18 +0900

    [mod] redis 비밀번호 설정 추가

--- api/src/main/java/com/wishboard/server/config/redis/RedisConfig.java
+++ api/src/main/java/com/wishboard/server/config/redis/RedisConfig.java
@@ -1,11 +1,13 @@
 package com.wishboard.server.config.redis;
 
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -17,25 +19,29 @@ import org.springframework.session.data.redis.config.ConfigureRedisAction;
 @EnableRedisRepositories
 public class RedisConfig {
 
-    private final RedisProperties redisProperties;
+	private final RedisProperties redisProperties;
 
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
-    }
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		var config = new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort());
+		if (redisProperties.getPassword() != null) {
+			config.setPassword(redisProperties.getPassword());
+		}
+		return new LettuceConnectionFactory(config);
+	}
 
-    @Primary
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        return redisTemplate;
-    }
+	@Primary
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new StringRedisSerializer());
+		return redisTemplate;
+	}
 
-    @Bean
-    public ConfigureRedisAction configureRedisAction() {
-        return ConfigureRedisAction.NO_OP;
-    }
+	@Bean
+	public ConfigureRedisAction configureRedisAction() {
+		return ConfigureRedisAction.NO_OP;
+	}
 }
