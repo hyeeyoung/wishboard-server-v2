c687846caec113bdf58a16f7839095df51c9b54f
hyejungg <kimhyejung12@naver.com>
2025-08-09 22:34:40 +0900

    [mod] 이미지 크기 1mb 까지만 가능하도록 수정

--- api/src/main/java/com/wishboard/server/image/application/service/service/S3Provider.java
+++ api/src/main/java/com/wishboard/server/image/application/service/service/S3Provider.java
@@ -11,13 +11,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Component
 public class S3Provider {
-	private static final int MAX_FILE_SIZE = 518400; // 720 * 720
+	private static final int MAX_FILE_BYTES = 1 * 1024 * 1024; // 1MB
 
 	private final S3FileStorageClient fileStorageClient;
 
 	public String uploadFile(UploadFileRequest request, MultipartFile file) {
 		request.validateAvailableContentType(file.getContentType());
-		request.validateImageMaxSize(file, MAX_FILE_SIZE);
+		request.validateImageMaxSize(file, MAX_FILE_BYTES);
 		String fileName = request.getFileNameWithBucketDirectory(file.getOriginalFilename());
 		fileStorageClient.uploadFile(file, fileName);
 		return fileStorageClient.getFileUrl(fileName);
