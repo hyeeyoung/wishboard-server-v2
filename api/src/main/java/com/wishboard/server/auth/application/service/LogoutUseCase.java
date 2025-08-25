ab32a3f3520055ee1bac74869c776c16cdeafe31
hyejungg <kimhyejung12@naver.com>
2025-08-26 00:24:08 +0900

    [fix] 로그아웃 시 동일 계정의 다른 기기 로그인 fcmToken 도 삭제하는 문제 수정

--- api/src/main/java/com/wishboard/server/auth/application/service/LogoutUseCase.java
+++ api/src/main/java/com/wishboard/server/auth/application/service/LogoutUseCase.java
@@ -20,7 +20,7 @@ public class LogoutUseCase {
 		var user = userReader.findById(userId);
 		var fcmTokens = user.getFcmTokens();
 		// deviceInfo가 일치하는 토큰이 있으면 그거만 삭제. 없으면 전체 삭제
-		boolean removed = fcmTokens.removeIf(token -> deviceInfo.equals(token.getFcmToken()));
+		boolean removed = fcmTokens.removeIf(token -> deviceInfo.equals(token.getDevice()));
 		if (!removed) {
 			fcmTokens.clear();
 		}
