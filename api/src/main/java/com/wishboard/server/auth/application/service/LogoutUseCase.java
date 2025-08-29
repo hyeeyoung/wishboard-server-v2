40e4c0f40a0e7c3fe44231815de76719a8140269
hyejungg <kimhyejung12@naver.com>
2025-08-29 14:59:26 +0900

    [fix] 전체 삭제 로직 수정

--- api/src/main/java/com/wishboard/server/auth/application/service/LogoutUseCase.java
+++ api/src/main/java/com/wishboard/server/auth/application/service/LogoutUseCase.java
@@ -7,7 +7,9 @@ import com.wishboard.server.user.application.service.support.UserReader;
 import com.wishboard.server.user.domain.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 @Transactional
@@ -22,7 +24,7 @@ public class LogoutUseCase {
 		// deviceInfo가 일치하는 토큰이 있으면 그거만 삭제. 없으면 전체 삭제
 		boolean removed = fcmTokens.removeIf(token -> token.getDevice().equals(deviceInfo));
 		if (!removed) {
-			fcmTokens.clear();
+			log.info("@@ Logout skip: device not found. userId={}, deviceId={}", userId, deviceInfo);
 		}
 		userRepository.save(user);
 	}
