9f14dea2f0d18d71f1c43f8d20899bfdbe5e2ac5
hyejungg <kimhyejung12@naver.com>
2025-08-29 12:36:20 +0900

    [fix] 로그아웃 시 전달 message 수정

--- api/src/main/java/com/wishboard/server/common/success/SuccessCode.java
+++ api/src/main/java/com/wishboard/server/common/success/SuccessCode.java
@@ -18,7 +18,7 @@ public enum SuccessCode {
 	// 인증
 	LOGIN_SUCCESS(OK, "로그인 성공입니다."),
 	REFRESH_TOKEN_SUCCESS(OK, "토큰 갱신 성공입니다."),
-	LOGOUT_SUCCESS(OK, "로그인 성공입니다."),
+	LOGOUT_SUCCESS(OK, "로그아웃 성공입니다."),
 
 	CHECK_EMAIL_SUCCESS(OK, "wishboard 이메일 확인 성공입니다."),
 	SIGNUP_SUCCESS(CREATED, "wishboard 회원가입 후 로그인 성공입니다."),
