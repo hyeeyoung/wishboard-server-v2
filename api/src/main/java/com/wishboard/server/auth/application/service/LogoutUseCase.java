8a0621fe1eeb03b6919daa78a4e39b9a849215a6
hyejungg <kimhyejung12@naver.com>
2025-08-29 14:43:42 +0900

    [fix] equals 검증 수정

--- api/src/main/java/com/wishboard/server/auth/application/service/LogoutUseCase.java
+++ api/src/main/java/com/wishboard/server/auth/application/service/LogoutUseCase.java
@@ -20,7 +20,7 @@ public class LogoutUseCase {
 		var user = userReader.findById(userId);
 		var fcmTokens = user.getFcmTokens();
 		// deviceInfo가 일치하는 토큰이 있으면 그거만 삭제. 없으면 전체 삭제
-		boolean removed = fcmTokens.removeIf(token -> deviceInfo.equals(token.getDevice()));
+		boolean removed = fcmTokens.removeIf(token -> token.getDevice().equals(deviceInfo));
 		if (!removed) {
 			fcmTokens.clear();
 		}
