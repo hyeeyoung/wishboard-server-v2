e89d99623ccd073090f1e27258bf2f64da42657b
hyejungg <kimhyejung12@naver.com>
2025-08-30 23:35:32 +0900

    [fix] 이미지가 없는 아이템을 폴더에 추가한 경우 폴더 썸네일 npe 방지

--- api/src/main/java/com/wishboard/server/folder/application/service/support/FolderThumbnailMapper.java
+++ api/src/main/java/com/wishboard/server/folder/application/service/support/FolderThumbnailMapper.java
@@ -5,24 +5,27 @@ import org.springframework.stereotype.Component;
 import com.wishboard.server.folder.application.dto.FolderDto;
 import com.wishboard.server.folder.domain.model.Folder;
 import com.wishboard.server.item.application.dto.FolderItemDto;
+import com.wishboard.server.item.domain.model.ItemImage;
 
 import jakarta.validation.constraints.NotNull;
 
 @Component
 public class FolderThumbnailMapper {
 	public FolderDto toDto(Folder folder, @NotNull FolderItemDto folderItemDto) {
-		if (folderItemDto.getItemCount() == 0L) {
-			return FolderDto.builder()
-				.id(folder.getId())
-				.folderName(folder.getFolderName())
-				.itemCount(0L)
-				.build();
-		}
-		return FolderDto.builder()
+		var builder = FolderDto.builder()
 			.id(folder.getId())
 			.folderName(folder.getFolderName())
-			.folderThumbnail(folderItemDto.getLastestItem().getImages().getFirst().getItemImageUrl())
-			.itemCount(folderItemDto.getItemCount())
-			.build();
+			.itemCount(folderItemDto.getItemCount());
+
+		if (folderItemDto.getItemCount() > 0) {
+			String thumbnail = folderItemDto.getLastestItem().getImages().stream()
+				.findFirst()
+				.map(ItemImage::getItemImageUrl)
+				.orElse(null);
+			builder.folderThumbnail(thumbnail);
+		}
+
+		return builder.build();
 	}
 }
+
