3149f3cd30917e6ea9c5c9f817e163c3cb9d925c
hyejungg <kimhyejung12@naver.com>
2025-11-20 00:01:42 +0900

    [fix] 푸쉬 알림 타이틀 수정

--- push/src/utils/strings.js
+++ push/src/utils/strings.js
@@ -1,6 +1,6 @@
 const Strings = {
   /* 알림 */
-  notiMessageTitle: 'dev상품일정알림',
+  notiMessageTitle: '상품일정알림',
   after30minutes: '30분 후에',
   notiMessageDescription: '아이템이 있어요!',
   notiMessageCountDescription: '상품 일정이 있어요!',
